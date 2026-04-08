ba7257afc35dc4d3ae5ac0e2376ef6c498122ec2
rafa0107 <rafael_blom@Rafael.localdomain>
2026-04-08 19:04:37 -0300

    Lógica de insercao de dados SQL

--- Storage/comment_repository.py
+++ Storage/comment_repository.py
@@ -1,1 +1,40 @@
 #Dados SQL de comentarios
+from src.Storage.database import get_connection
+
+class CommentRepository:
+    def save_comment(self, comment_data):
+
+        if not comment_data or not comment_data.get('text_display'):
+            print("Comentário ignorado (vazio ou inválido).")
+            return
+
+        conn = None
+        cursor = None
+
+        try:
+            conn = get_connection()
+            cursor = conn.cursor()
+            insert_query = """
+                INSERT IGNORE INTO comments (comment_id, video_id, author_display_name, text_display, published_at, like_count)
+                VALUES (%s, %s, %s, %s, %s, %s)
+            """
+            cursor.execute(insert_query, (
+                comment_data.get('comment_id'),
+                comment_data.get('video_id'),
+                comment_data.get('author_display_name'),
+                comment_data.get('text_display'),
+                comment_data.get('published_at'),
+                comment_data.get('like_count'),
+            ))
+
+            conn.commit()
+            print(f"Comentário '{comment_data.get('comment_id')}' salvo com sucesso.")
+
+        except Exception as e:
+            print(f"Erro ao salvar comentário: {e}")
+
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                conn.close()
--- Storage/video_repository.py
+++ Storage/video_repository.py
@@ -1,1 +1,38 @@
 #Dados SQL de videos
+from src.Storage.database import get_connection
+
+class VideoRepository:
+      
+    def save_video(self, video_data):
+
+        conn = None
+        cursor = None
+
+        try:
+            conn = get_connection()
+            cursor = conn.cursor()
+            insert_query = """
+                INSERT IGNORE INTO videos (video_id, title, published_at, channel_id, channel_title)
+                VALUES (%s, %s, %s, %s, %s)
+            """
+            cursor.execute(insert_query, (
+                video_data.get('video_id'),
+                video_data.get('title'),
+                video_data.get('published_at'),
+                video_data.get('channel_id'),
+                video_data.get('channel_title')
+            ))
+
+            conn.commit()
+            print(f"Vídeo '{video_data.get('title')}' salvo com sucesso.")
+
+        except Exception as e:
+            print(f"Erro ao salvar vídeo: {e}")
+
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                conn.close()
+
+
